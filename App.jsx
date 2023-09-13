d027ea7e62cc97f640d4492e148b3c66039151ea
Estefane1992 <fannyczs2012@gmail.com>
2023-09-13 15:52:26 -0300

    atualizando arquivos

--- App.jsx
+++ App.jsx
@@ -1,7 +1,16 @@
-import { View } from "react-native";
+import { StatusBar } from "react-native";
+import Home from "./src/screens/Home";
+
 
 export default function App(){
   return(
-    <View></View>
+    <>
+    <StatusBar 
+      barStyle="light-content"
+      backgroundColor="transparent"
+      translucent
+    />
+      <Home />
+    </>
   )
 }
